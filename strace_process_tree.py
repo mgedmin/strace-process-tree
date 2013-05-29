b5b641da6794416eba522be945ed91a0877a5b05
Marius Gedminas <marius@gedmin.as>
2013-05-29 16:46:57 +0300

    Fix strace files that have two spaces between pid and event

--- strace_process_tree.py
+++ strace_process_tree.py
@@ -12,7 +12,7 @@ import fileinput
 from collections import defaultdict
 
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'Marius Gedminas <marius@gedmin.as>'
 __url__ = 'https://gist.github.com/mgedmin/4953427'
 __licence__ = 'GPL v2 or later' # or ask me for MIT
@@ -25,6 +25,7 @@ def events(stream):
     for line in stream:
         pid, spaces, event = line.rstrip().partition(' ')
         pid = int(pid)
+        event = event.lstrip()
         m = RESUMED_PREFIX.match(event)
         if m is not None:
             event = pending.pop(pid) + event[len(m.group()):]
