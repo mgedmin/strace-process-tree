2b8e51d4aa1af57125497c0799332c356370b644
Marius Gedminas <marius@gedmin.as>
2018-01-19 01:59:55 +0200

    New default brief output that lists command lines
    Use -v to see old-style full system call arguments.
    
    There's also an important bugfix (parent losing the clone race against
    the new child's execve could clobber its command line in the tree
    output).

--- strace_process_tree.py
+++ strace_process_tree.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 """
 Usage:
-  strace-process-tree [filename]
+  strace-process-tree filename
 
 Read strace -f output and produce a process tree.
 
@@ -12,26 +12,34 @@ Recommended strace options for best results:
 
 """
 
+import argparse
 import re
-import fileinput
+import string
 from collections import defaultdict
 
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 __author__ = 'Marius Gedminas <marius@gedmin.as>'
 __url__ = 'https://gist.github.com/mgedmin/4953427'
 __licence__ = 'GPL v2 or later' # or ask me for MIT
 
 
 def events(stream):
-    RESUMED_PREFIX = re.compile('<... \w+ resumed> ')
+    RESUMED_PREFIX = re.compile(r'<... \w+ resumed> ')
     UNFINISHED_SUFFIX = ' <unfinished ...>'
-    DURATION_SUFFIX = re.compile(' <\d+([.]\d+)?>$')
-    TIMESTAMP = re.compile('^\d+([.]\d+)?\s+')
+    DURATION_SUFFIX = re.compile(r' <\d+([.]\d+)?>$')
+    TIMESTAMP = re.compile(r'^\d+([.]\d+)?\s+')
     pending = {}
     for line in stream:
-        pid, spaces, event = line.rstrip().partition(' ')
-        pid = int(pid)
+        pid, space, event = line.rstrip().partition(' ')
+        try:
+            pid = int(pid)
+        except ValueError:
+            raise SystemExit(
+                "This does not look like a log file produced by strace -f:\n\n"
+                "  %s\n"
+                "There should've been a PID at the beginning of the line."
+                % line)
         event = event.lstrip()
         event = TIMESTAMP.sub('', event)
         event = DURATION_SUFFIX.sub('', event)
@@ -58,6 +66,9 @@ class ProcessTree:
             self.roots.add(pid)
             self.all.add(pid)
 
+    def has_name(self, pid):
+        return pid in self.names
+
     def set_name(self, pid, name):
         self.make_known(pid)
         self.names[pid] = name
@@ -79,41 +90,141 @@ class ProcessTree:
                 s, cs = '  ├─', '  │ '
             else:
                 s, cs = '  └─', '    '
-            r.append(indent + s + '{} {}\n'.format(pid, self.names.get(pid, '')))
-            r.append(self._format(sorted(self.children.get(pid, [])),
-                                  indent+cs, level+1))
+            name = self.names.get(pid, '')
+            children = sorted(self.children.get(pid, []))
+            if children:
+                ccs = '  │ '
+            else:
+                ccs = '    '
+            name = name.replace('\n', '\n' + indent + cs + ccs + '    ')
+            r.append(indent + s + '{} {}\n'.format(pid, name))
+            r.append(self._format(children, indent+cs, level+1))
 
         return ''.join(r)
 
-    def __str__(self):
+    def format(self):
         return self._format(sorted(self.roots))
+
+    def __str__(self):
+        return self.format()
 
 
 def simplify_syscall(event):
     # clone(child_stack=0x..., flags=FLAGS, parent_tidptr=..., tls=..., child_tidptr=...) => clone(FLAGS)
     if event.startswith('clone('):
         event = re.sub('[(].*, flags=([^,]*), .*[)]', r'(\1)', event)
-    return event
+    return event.rstrip()
+
+
+def extract_command_line(event):
+    # execve("/usr/bin/foo", ["foo", "bar"], [/* 45 vars */]) => foo bar
+    if event.startswith('clone('):
+        if 'CLONE_THREAD' in event:
+            return '(thread)'
+        else:
+            return '...'
+    elif event.startswith('execve('):
+        command = re.sub(r'^execve\([^[]*\[', '', re.sub(r'\], \[/\* \d+ vars \*/\]\)$', '', event.rstrip()))
+        command = parse_argv(command)
+        return format_command(command)
+    else:
+        return event.rstrip()
+
+
+ESCAPES = {
+    'n': '\n',
+    'r': '\r',
+    't': '\t',
+    'b': '\b',
+    '0': '\0',
+    'a': '\a',
+}
+
+
+def parse_argv(s):
+    # '"foo", "bar"..., "baz", "\""' => ['foo', 'bar...', 'baz', '"']
+    it = iter(s + ",")
+    args = []
+    for c in it:
+        if c == ' ':
+            continue
+        assert c == '"'
+        arg = []
+        for c in it:
+            if c == '"':
+                break
+            if c == '\\':
+                c = next(it)
+                arg.append(ESCAPES.get(c, c))
+            else:
+                arg.append(c)
+        c = next(it)
+        if c == ".":
+            arg.append('...')
+            c = next(it)
+            assert c == "."
+            c = next(it)
+            assert c == "."
+            c = next(it)
+        args.append(''.join(arg))
+        assert c == ','
+    return args
+
+
+SHELL_SAFE_CHARS = set(string.ascii_letters + string.digits + '%+,-./:=@^_~')
+SHELL_SAFE_QUOTED = SHELL_SAFE_CHARS | set("!#&'()*;<>?[]{|} \t\n")
+
+
+def format_command(command):
+    return ' '.join(map(pushquote, (
+        arg if all(c in SHELL_SAFE_CHARS for c in arg) else
+        '"%s"' % arg if all(c in SHELL_SAFE_QUOTED for c in arg) else
+        "'%s'" % arg.replace("'", "'\\''")
+        for arg in command
+    )))
+
+
+def pushquote(arg):
+    return re.sub('''^(['"])(--[a-zA-Z0-9_-]+)=''', r'\2=\1', arg)
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="""
+            Read strace -f output and produce a process tree.
+
+            Recommended strace options for best results:
+
+                strace -f -e trace=process -s 1024 -o FILENAME COMMAND
+            """)
+    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='more verbose output')
+    parser.add_argument('filename', type=argparse.FileType('r'),
+                        help='strace log to parse (use - to read stdin)')
+    args = parser.parse_args()
+
     tree = ProcessTree()
 
-    for pid, event in events(fileinput.input()):
+    mogrifier = simplify_syscall if args.verbose else extract_command_line
+
+    for pid, event in events(args.filename):
         if event.startswith('execve('):
             args, equal, result = event.rpartition(' = ')
             if result == '0':
-                name = simplify_syscall(args)
+                name = mogrifier(args)
                 tree.set_name(pid, name)
         if event.startswith(('clone(', 'fork(', 'vfork(')):
             args, equal, result = event.rpartition(' = ')
             if result.isdigit():
                 child_pid = int(result)
-                name = simplify_syscall(args)
-                tree.set_name(child_pid, name)
+                name = mogrifier(args)
+                if not tree.has_name(child_pid):
+                    tree.set_name(child_pid, name)
                 tree.add_child(pid, child_pid)
 
-    print(str(tree).rstrip())
+    print(tree.format().rstrip())
+
 
 if __name__ == '__main__':
     main()
